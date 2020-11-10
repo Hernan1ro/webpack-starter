83d71c75d117972fb3adec7e4fd74eefd40fca27
Hernán Mercado <hernandmf@gmail.com>
2020-11-10 14:22:02 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { saludar } from "./js/componentes.js";
+import "./styles.css";
+
+const nombre = "Fernando!! Best courses of udemy!!";
+
+saludar(nombre);
---
+++ src/js/componentes.js
@@ -0,0 +1,7 @@
+import "../css/componentes.css";
+export const saludar = (nombre) => {
+  console.log("Creando etiqueta h1");
+  const h1 = document.createElement("h1");
+  h1.innerText = `Hola, ${nombre}, cómo estás`;
+  document.body.append(h1);
+};
